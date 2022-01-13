bc3312d44b98248a29f8e3e52d945127b2655012
akvamarinel <m.karaseva2000@mail.ru>
2022-01-13 09:54:54 +0300

    fix

--- solution/src/bmp.c
+++ solution/src/bmp.c
@@ -8,8 +8,14 @@
 #include <malloc.h>
 #include <stdbool.h>
 
+uint8_t get_padding(uint64_t width){
+    return(4 - (width * sizeof (struct pixel)) % 4) % 4;
+}
+
 struct bmp_header create_header(struct image * image){
-    uint8_t padding = (4 - (image->width * sizeof (struct pixel)) % 4) % 4;
+
+    uint8_t padding = get_padding(image->width);
+            //(4 - (image->width * sizeof (struct pixel)) % 4) % 4;
     size_t size = (image->height * ((image->width) + padding)) * sizeof(struct pixel);
     struct bmp_header new_header = {
             .bfType = SIG_FORM,
@@ -49,12 +55,18 @@ void get_pixels (FILE * file, struct image * image, uint64_t width, uint64_t hei
         fseek(file, padding, SEEK_CUR);
     }
 }
+
+
 void write_pixels(FILE* file,struct image* image, uint64_t width, uint64_t height, uint8_t padding) {
     uint8_t buf_pad [4] = {0};
     for (size_t i = 0; i < height; ++i) {
         fwrite(&(image->data[i * width]), sizeof(struct pixel), width, file);
         fwrite(&buf_pad, 1, padding, file);
     }
+}
+void make_free(struct image * old_image, struct image * new_image){
+    free(old_image->data);
+    free(new_image->data);
 }
 
 
@@ -73,7 +85,8 @@ enum read_status from_bmp(FILE * file, struct image * image) {
     *image = create_image(header.biWidth, header.biHeight);
 
 
-    uint8_t padding = (4 - (image->width * sizeof (struct pixel)) % 4) % 4;
+    uint8_t padding = get_padding(image->width);
+            //(4 - (image->width * sizeof (struct pixel)) % 4) % 4;
     if(fseek(file, header.bOffBits, SEEK_SET) != 0) { //
        print_read_error(READ_INVALID_BITS);
        return READ_INVALID_BITS;
@@ -81,6 +94,7 @@ enum read_status from_bmp(FILE * file, struct image * image) {
     get_pixels(file, image, image->width, image->height, padding);
     return READ_OK;
 }
+
 
 enum write_status to_bmp_and_rotate(FILE * file, struct image * old_image) {
 
@@ -93,12 +107,10 @@ enum write_status to_bmp_and_rotate(FILE * file, struct image * old_image) {
        print_write_error(WRITE_ERROR);
        return WRITE_ERROR;
     }
-    uint8_t padding = (4 - (new_image.width * sizeof (struct pixel)) % 4) % 4;
+    uint8_t padding = get_padding(new_image.width);
     write_pixels(file, &new_image, new_image.width, new_image.height, padding);
 
-
-    free(new_image.data);
-    free(old_image->data);
+    make_free(old_image, &new_image);
     return WRITE_OK;
 }
 
