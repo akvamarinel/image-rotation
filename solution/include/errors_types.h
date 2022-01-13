4656b059d40a45f718fea0b52c307863e9c901a3
akvamarinel <m.karaseva2000@mail.ru>
2022-01-13 19:46:13 +0300

    fix

---
+++ solution/include/errors_types.h
@@ -0,0 +1,15 @@
+//
+// Created by mashusik on 13.01.2022.
+//
+
+#ifndef ERRORS_TYPES_H
+#define ERRORS_TYPES_H
+
+#include "bmp.h"
+#include "serialization.h"
+
+extern const char* read_msg[] ;
+extern const char* write_msg[];
+extern const char* open_msg[] ;
+
+#endif //UNTITLED7_ERRORS_TYPES_H
---
+++ solution/src/errors_types.c
@@ -0,0 +1,22 @@
+#include "errors_types.h"
+
+ const char* read_msg[] = {
+        [READ_OK] = "read ok\n",
+        [READ_ERROR] = "read error\n",
+        [READ_INVALID_SIGNATURE] = "read invalid signature\n",
+        [READ_INVALID_BITS] = "read invalid bits\n",
+        [READ_INVALID_PLANES] = "read invalid planes\n",
+        [READ_ALLOC_ERROR] = "allocation error\n",
+        [READ_INVALID_HEADER] = "read invalid size if header\n",
+
+};
+
+ const char* write_msg[] = {
+        [WRITE_OK] = "write ok\n",
+        [WRITE_ERROR] = "write erros\n"
+};
+
+ const char* open_msg[] = {
+        [OPEN_OK] = "open ok\n",
+        [OPEN_ERROR] = "open error\n"
+};
