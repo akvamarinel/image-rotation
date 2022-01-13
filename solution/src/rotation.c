ea73c6f2aa4c5ecc8e1d2b4df36c2bb14834315a
akvamarinel <m.karaseva2000@mail.ru>
2022-01-13 20:02:52 +0300

    added static fucntion

--- solution/src/rotation.c
+++ solution/src/rotation.c
@@ -5,11 +5,18 @@
 #include "rotation.h"
 #include <stdint.h>
 
+static uint64_t get_x(uint64_t i, uint64_t j, uint64_t width){
+    return j * width + (width - i - 1);
+};
+static uint64_t get_y (uint64_t i, uint64_t j, uint64_t width){
+    return i * width + j;
+};
+
 void rotate(struct image * old_image, struct image new_image) {
     for ( uint64_t i = 0; i < old_image->height; i++) {
         for (uint64_t j = 0; j < old_image->width; j++) {
-            new_image.data[j * new_image.width + (new_image.width - i -1)] = old_image->data[i * old_image->width + j];
+            new_image.data[get_x(i, j, new_image.width)] = old_image->data[get_y(i, j, old_image->width)];
         }
     }
 
-}
+};
